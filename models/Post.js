914dc3a7b831bd8f5e4c5f82b86822926342f99a
“Levar <levar.pitts@gmail.com>
2024-05-06 01:11:48 -0400

    Making changes to API

--- models/Post.js
+++ models/Post.js
@@ -17,6 +17,10 @@ const PostSchema = new mongoose.Schema(
             type: Array,
             default: [],
 
+        },
+        eventId: {
+            type: String,
+            default: "",
         }
     
     }, 
