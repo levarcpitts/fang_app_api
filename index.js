612b5c3bc8787c94f69d75cd5bd575fae32a4648
“Levar <levar.pitts@gmail.com>
2024-05-13 01:39:30 -0400

    Follow/Unfollow users

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const helmet = require("helmet");
 const morgan = require("morgan");
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth")
-const postRoute = require("./routes/posts")
+const postRoute = require("./routes/posts");
+const multer = require("multer");
+const path = require("path")
 
 
 dotenv.config();
@@ -18,10 +20,32 @@ mongoose.connect(process.env.MONGO_URL,)
   .catch(err => console.error("Could not connect to MongoDB", err));
 
 
+  app.use("/images", express.static(path.join(__dirname, "public/images")))
+
+
  //middleware
  app.use(express.json());
  app.use(helmet());
  app.use(morgan("common"));
+
+ const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  try {
+    return res.status(200).json("File uploded successfully");
+  } catch (error) {
+    console.error(error);
+  }
+});
+
 
  app.use("/api/users", userRoute);
  app.use("/api/auth", authRoute);
